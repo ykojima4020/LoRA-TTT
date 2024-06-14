d3cde56058c520666f23ac0192dacc0b29871574
Yuto Kojima <yuto.kojima@sony.com>
2024-06-13 18:24:12 -0700

    Combined with TPT

--- tta/tta.py
+++ tta/tta.py
@@ -131,8 +131,9 @@ class TPTMAETTARunner():
 
 class MEMLoRATTARunner():
 
-    def __init__(self):
-        print(f'{type(self)} created.')
+    def __init__(self, mae=True, tpt=False):
+        self._mae = mae
+        self._tpt = tpt
 
     def __call__(self, factory, status,
                  tta_dataset, prompts, classes, config,
@@ -141,30 +142,46 @@ class MEMLoRATTARunner():
         model = model.to(device)
 
         # [NOTE]: trainable parameters
-        for name, param in model.image_encoder.named_parameters():
-            if 'lora' in name:
-                param.requires_grad = True
+        if self._tpt:
+            model.clip.prompt_learner.ctx.requires_grad = True
+        if self._mae:
+            for name, param in model.image_encoder.named_parameters():
+                if 'lora' in name:
+                    param.requires_grad = True
 
         for name, param in model.named_parameters():
             print(f'{name}: {param.requires_grad}')
 
 
-        text_embeddings = zeroshot_weights(model.clip, tokenizer, classes, prompts, device)
-        # [NOTE]: MAE optimizer, update only image encoder
-        if config.mae.optimizer == 'adam':
-            eps = 1e-8
-            optimizer = torch.optim.AdamW(model.image_encoder.parameters(),
-                eps=eps, lr=config.mae.lr, betas=(0.9, 0.95), weight_decay=config.mae.weight_decay)
-        elif config.mae.optimizer == 'sgd':
-            optimizer = torch.optim.SGD(model.image_encoder.parameters(), lr=config.mae.lr, weight_decay=config.mae.weight_decay)
-        else:
-            raise TypeError
+        if self._tpt:
+            # [NOTE]: fixed parameters for TPT
+            arch = 'ViT-B/16'
+            trainable_param = model.clip.prompt_learner.parameters()
+            optimizer = torch.optim.AdamW(trainable_param, config.tpt.lr)
+            optim_state = deepcopy(optimizer.state_dict())
+            model.clip.reset_classnames(classes, arch)
+
+            # setup automatic mixed-precision (Amp) loss scaling
+            scaler = torch.cuda.amp.GradScaler(init_scale=1000)
+
+        if self._mae:
+            text_embeddings = zeroshot_weights(model.clip, tokenizer, classes, prompts, device)
+            # [NOTE]: MAE optimizer, update only image encoder
+            if config.mae.optimizer == 'adam':
+                eps = 1e-8
+                mae_optimizer = torch.optim.AdamW(model.image_encoder.parameters(),
+                        eps=eps, lr=config.mae.lr, betas=(0.9, 0.95), weight_decay=config.mae.weight_decay)
+            elif config.mae.optimizer == 'sgd':
+                mae_optimizer = torch.optim.SGD(model.image_encoder.parameters(), lr=config.mae.lr, weight_decay=config.mae.weight_decay)
+            else:
+                raise TypeError
 
 
         tta_data_loader = torch.utils.data.DataLoader(
                     tta_dataset,
                     batch_size=1, shuffle=False,
                     num_workers=num_workers, pin_memory=pin_memory)
+
 
         top1 = AverageMeter('Acc@1', ':6.2f', Summary.AVERAGE)
         top5 = AverageMeter('Acc@5', ':6.2f', Summary.AVERAGE)
@@ -176,6 +193,10 @@ class MEMLoRATTARunner():
 
         selection_p = 0.1
 
+        model.eval()
+        with torch.no_grad():
+            model.clip.reset()
+
         for i, (images, target) in tqdm(enumerate(tta_data_loader)):
 
             for k in range(len(images)):
@@ -184,26 +205,46 @@ class MEMLoRATTARunner():
             image = images[0]
             images = torch.cat(images, dim=0)
 
-            # [TODO]: should load only LoRA and Decoder, not update text_encoder
-            model.mae.load_state_dict(status)
-            for j in range(config.mae.epochs):
-                image_features = model.clip.image_encode(images)
-                image_features = image_features / image_features.norm(dim=-1, keepdim=True)
-                output = model.clip.logit_scale.exp() * (image_features @ text_embeddings)
-                output, _ = select_confident_samples(output, selection_p)
-                loss = avg_entropy(output)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            if self._tpt:
+                # reset the tunable prompt to its initial state
+                if config.tpt.epochs > 0:
+                    with torch.no_grad():
+                        model.clip.reset()
 
+                # [NOTE]: I don't know why optimizer is loaded here.
+                optimizer.load_state_dict(optim_state)
+                test_time_tuning(model.clip, images, optimizer, scaler, config.tpt)
+
+            # [NOTE]: MAE TTA
+            if self._mae:
+
+                # [TODO]: should load only LoRA and Decoder, not update text_encoder
+                model.mae.load_state_dict(status)
+                for j in range(config.mae.epochs):
+                    if self._tpt:
+                        output = model.clip(image)
+                    else:
+                        image_features = model.clip.image_encode(images)
+                        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+                        output = model.clip.logit_scale.exp() * (image_features @ text_embeddings)
+                    output, _ = select_confident_samples(output, selection_p)
+                    loss = avg_entropy(output)
+                    mae_optimizer.zero_grad()
+                    loss.backward()
+                    mae_optimizer.step()
+
+ 
             # [NOTE]: inference
             model.eval()
             model.clip = model.clip.to(device) # why?
             with torch.no_grad():
                 with torch.cuda.amp.autocast():
-                    image_features = model.clip.image_encode(image)
-                    image_features /= image_features.norm(dim=-1, keepdim=True)
-                    output = image_features @ text_embeddings
+                    if self._tpt:
+                        output = model.clip(image)
+                    else:
+                        image_features = model.clip.image_encode(image)
+                        image_features /= image_features.norm(dim=-1, keepdim=True)
+                        output = image_features @ text_embeddings
 
             # measure accuracy and record loss
             acc1, acc5 = accuracy(output, target, topk=(1, 5))
@@ -214,7 +255,6 @@ class MEMLoRATTARunner():
                 progress.display(i)
 
         return top1.avg.item(), top5.avg.item()
-
 
 
 def test_time_tuning(clip, inputs, optimizer, scaler, config):
